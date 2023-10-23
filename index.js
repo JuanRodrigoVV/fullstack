541a1ee08dab801a3172f05d48dc280a6ed1a11d
Rodrigo Vargas <rodrigo.juan.vargas@gmail.com>
2023-10-23 10:48:44 -0300

    part3.3

--- index.js
+++ index.js
@@ -1,101 +1,108 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.json())
 const cors = require('cors')
+const Note = require('./models/note')
+// const dotenv = require("dotenv")
+// dotenv.config()
 
 
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
 
 app.use(cors())  
+app.use(express.static('dist'))
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+
+
+
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
+
+
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => {
-        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-        return note.id === id
-      })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
       if (note) {
         response.json(note)
       } else {
         response.status(404).end()
       }
-    console.log(request.params``)
-    response.json(note)
-  })
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-})  
-
-app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const newNote = notes.find(note => note.id === id)
-  // const newNote = request.body
-  let newNotes = [...notes]
-  let important = newNotes[id - 1].important
-  newNotes[id - 1] = {...newNotes[id - 1], important: !important}
-  notes = newNotes
-
-  
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body  
+  const note = {
+    content: body.content,
+    important: body.important,
   }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+
+  Note.findByIdAndUpdate(
+    request.params.id,  
+    { content, important },
+    { new: true, runValidators: true, context: 'query' })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
   })
 
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // handler of requests with unknown endpoint
+  app.use(unknownEndpoint)
+
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).send({error: error.message})
+    }
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware.
+  app.use(errorHandler)
